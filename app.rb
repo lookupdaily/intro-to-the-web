cd768439938e424e5b20d54446345a78bc241d76
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-02-03 15:48:45 +0000

    Added index.erb

--- app.rb
+++ app.rb
@@ -3,16 +3,7 @@ require 'sinatra'
 set :session_secret, 'super secret'
 
 get '/' do
-  "hello!"
-end
-
-get '/secret' do
-  'shhhhh'
-end
-
-get '/cat' do
-  "<div style='border:dashed red;'> 
-   <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  @name_sample = ["Ben", "Liz", "Sophie"].sample
+  erb :index
 end
 
