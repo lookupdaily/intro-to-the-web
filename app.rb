55b67dfba7f1f5455c6cd43a9bf217023c541605
Liz Daly <liz7daly@gmail.com>
2020-02-03 16:17:55 +0000

    Add form for cat name

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require 'sinatra'
 
 set :session_secret, 'super secret'
 
-get '/' do
-  @name_sample = ["Ben", "Liz", "Sophie"].sample
+get '/random-cat' do
+  @name = ["Ben", "Liz", "Sophie"].sample
   erb :index
 end
 
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
+end
